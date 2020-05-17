56571f96cb2c104c5abc42a3b2cce25c3b837b8b
Diliana <51090378+Dil-iana@users.noreply.github.com>
2020-05-17 18:53:57 +0200

    Add files via upload

---
+++ Week 5/Julia_Matrix.jl
@@ -0,0 +1,68 @@
+#Exercise #3 (Julia)
+X = [2.6 7.8; 6.8 9.0; 7.3 1.4; 4.6 3.8]
+
+maximum(X)
+
+
+#Exercise #4 (Julia)
+
+M1 = [5.3 6.7 3.2; 9.2 2.7 1.9; 8.3 7.5 2.9]
+
+
+
+
+M2 = [1.9 5.9 7.3; 8.3 4.2 4.9; 6.2 2.9 0.1]
+
+
+
+#Addition
+M1 + M2
+
+
+
+
+#Substraction
+M1 - M2
+
+
+
+
+#The incorrect way to multiply two matrices
+M1 * M2
+
+
+
+#The correct way to multiply two matrices
+M1.*M2
+
+
+
+
+#Division
+M1\M2
+
+
+
+
+# 3x3 integer matrix
+
+M3 = [1 5 7; 9 3 2; 4 1 7]
+
+
+
+#M3 * 2
+M3 * 2
+
+
+
+
+#M3 / 2
+M3 / 2
+
+
+
+
+#3x4 matrix multiplied by a vector
+M4 = [5 3 1 6;6 2 5 4;7 1 4 2]
+V = [4;1;5;8]
+M4 * V
